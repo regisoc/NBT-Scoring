6939ffbee96c15ad71b553bdae2caa2f9327519e
regisoc <regis.ongaro-carcy@mcin.ca>
2025-06-15 21:32:18 -0400

    update file name and parsing

--- score_data_batch.R
+++ score_data_batch.R
@@ -66,17 +66,24 @@ all_output<-data.frame()
 for(i in 1:length(item_export_files)) {
   
   # scoring by file
+  # ex of filename
+  # ncl_ch_nbtb_QIUMN0013_523319_P06_ScoresExport_2024-03-22T153050.csv
+  # ncl_ch_nbtb_QINWU0022_523520_P06_RegistrationExport_2024-04-30T193955.csv
   file_name=item_export_files[i]
   print(paste0("------ Scoring: ",file_name))
   
+  # filename without extension
+  barename <- str_split(file_name, pattern='.csv')[[1]][1]
+
   # match_id format "PSCID_CANDID_VISIT" e.g. "DCC090_123456_V01"
-  t <- str_split(file_name,pattern='_')[[1]]
+  t <- str_split(barename,pattern='_')[[1]]
   instrument_name <- paste0(t[1], '_', t[2], '_', t[3]) # ncl_ch_nbtb
   match_id        <- paste0(t[4], '_', t[5], '_', t[6]) # "PSCID_CANDID_VISIT"
+  timestamp       <- t[8]                               # 2024-04-30T193955 => not ISO-8601 compliant.
 
   # match_id<-str_split(file_name,pattern='_')[[1]][2]
-  item_export         <- read.csv(paste0(item_export_path,instrument_name,'_',match_id,'_ItemExportNarrow.csv'))
-  registration_export <- read.csv(paste0(registration_export_path,instrument_name,'_',match_id,'_RegistrationExportNarrow.csv'))
+  item_export         <- read.csv(paste0(item_export_path,instrument_name,'_',match_id,'_ItemExportNarrow_',timestamp,'.csv'))
+  registration_export <- read.csv(paste0(registration_export_path,instrument_name,'_',match_id,'_RegistrationExportNarrow_',timestamp,'.csv'))
 
   # pull age information
   age<-registration_export%>% 
